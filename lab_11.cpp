906274563d47a4ddb91a3b14469dcddbd22ab3a0
Tanmayee Chalamalasetti <tanmayeesc@MacBookPro.attlocal.net>
2025-09-21 00:59:05 -0700

    came back after a break, but add some trouble pushing it then

--- lab_11.cpp
+++ lab_11.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <string>
 #include <vector>
-#
 using namespace std;
 
-struct Activites {}
+// This struct represents a Villager in the game Stardew Valley.
+struct Villager {
+  std::string name;                        // Villager's name
+  int friendshipLevel;                     // Friendship level from 0 to 10
+  std::vector<std::string> favoriteGifts;  // Dynamic array of favorite gifts
+};
+
+void addVillager(vector<Villager>& villagers, string name, int friendshipLevel,
+                 vector<string> gifts) {
+  Villager v;
+  v.name = name;
+  v.friendshipLevel = friendshipLevel;
+  v.favoriteGifts = gifts;
+}
